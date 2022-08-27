b262f88c5042462904bd888fc3b1085b22e7549a
youjae <youjae0803@gmail.com>
2022-08-27 15:40:30 +0900

    hmm to Difficult

--- DavinchCode/app.js
+++ DavinchCode/app.js
@@ -6,6 +6,7 @@ class Card {
   constructor(c, n) {
     this.color = c;
     this.number = n;
+    this.open = false;
   }
 
   static compare(a, b) {
@@ -44,13 +45,114 @@ class Deck {
 
   pop(color) {
     if (color == "white") {
-      return this.whiteCards.pop();
+      return this.whiteCards.pop("white");
     } else {
-      return this.blackCards.pop();
+      return this.blackCards.pop("black");
     }
   }
 }
 
-class Player {}
+class Play {}
 
-class Computer {}
+class Player {
+  constructor() {
+    this.cards = [];
+    this.openCards = [];
+    this.LastPickedCard = new Card();
+  }
+
+  pick(color, deck) {
+    if (color === "white" && deck.whiteCards.length >= 1) {
+      this.cards.push(deck.whiteCards.pop("white"));
+      this.LastPickedCard = this.cards[this.cards.length - 1];
+    } else if (color === "black" && deck.blackCards.length >= 1) {
+      this.cards.push(deck.blackCards.pop("black"));
+      this.LastPickedCard = this.cards[this.cards.length - 1];
+    } else {
+      this.LastPickedCard = null;
+    }
+  }
+}
+
+class Computer {
+  constructor() {
+    this.cards = [];
+    this.openCards = [];
+    this.LastPickedCard = new Card();
+    this.myGuess = []; //{number, pos, turnNumber}
+    this.guessOfPlayer = []; //{number, pos, turnNumber}를 저장
+    this.addedCardPosOfPlayer = []; // player가 뽑고 넣은 위치를 기억
+  }
+
+  guess(playerCards, openCardsOfPlayer) {
+    this.initRange(openCardsOfPlayer);
+
+    possibility = [];
+    for (let i = 0; i < playerCards.length; ++i) {
+      if (playerCards[i].color === "white") {
+        possibility[i] = [...this.canWhite];
+      } else {
+        possibility[i] = [...this.canBlack];
+      }
+    }
+  }
+
+  initRange(openCardsOfPlayer) {
+    let whiteCardList = [];
+    let blackCardList = [];
+
+    for (let i = 0; i < 13; ++i) {
+      whiteCardList.push(true);
+      blackCardList.push(true);
+    }
+
+    for (const item of openCardsOfPlayer) {
+      if (item.color === "white")
+        if (Number.isInteger(item.number)) whiteCardList[item.number] = false;
+        else whiteCardList[12] = false;
+      else {
+        if (Number.isInteger(item.number)) blackCardList[item.number] = false;
+        else blackCardList[12] = false;
+      }
+    }
+
+    for (const item of this.cards) {
+      if (item.color === "white")
+        if (Number.isInteger(item.number)) whiteCardList[item.number] = false;
+        else whiteCardList[12] = false;
+      else {
+        if (Number.isInteger(item.number)) blackCardList[item.number] = false;
+        else blackCardList[12] = false;
+      }
+    }
+
+    this.canWhite = [];
+    this.canBlack = [];
+
+    for (let i = 0; i < whiteCardList.length; ++i) {
+      if (whiteCardList[i]) canWhite.push(i);
+    }
+
+    for (let i = 0; i < blackCardList.length; ++i) {
+      if (blackCardList[i]) canBlack.push(i);
+    }
+  }
+}
+
+class GameManager {
+  constructor() {
+    this.player = new Player();
+    this.computer = new Computer();
+    this.deck = new Deck();
+    this.currentTurn = this.computer;
+  }
+
+  check(card, submit) {
+    if (card.number === submit) {
+      card.open = true;
+    } else {
+      if (this.currentTurn.LastPickedCard !== null)
+        this.currentTurn.LastPickedCard.open = true;
+    }
+  }
+}
