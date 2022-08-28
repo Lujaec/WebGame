68713db5b4d324212f5190aa542b011ceb76b88f
박진우 <wlsdndml213@naver.com>
2022-08-28 15:37:12 +0900

    before click Event capsulation

--- quoridor/Board.js
+++ quoridor/Board.js
@@ -2,38 +2,39 @@ class Board{
     constructor(){
 
     }
-    getPlayerBoard(){
-        let board=document.createElement('table');
-				board.id="playerBoard";
-				console.log("getPlayerBoard");
-				
-				for(let i=0;i<9;i++){
-					let boardRow = document.createElement('tr');
-					for(let j=0;j<9;j++){
-						let boardUnit = getBoardUnit(i,j,'boardUnitPlayer');
-						boardRow.append(boardUnit);
-					}
-					board.append(boardRow);
-				}
-				document.body.append(board);
-    }
-		getObstacleBoard(){
-			let board=document.createElement('table');
-				board.id="obstacleBoard";
-				console.log("getObstacleBoard");
-				for(let i=0;i<8;i++){
-					let boardRow = document.createElement('tr');
-					for(let j=0;j<8;j++){
-						let boardUnit=getBoardUnit(i,j,'boardUnitObstacle');
-						boardRow.append(boardUnit);
-					}
-					board.append(boardRow);
-				}
-				document.body.append(board);
+    initBoard() {
+			initPlayerBoard();
+			initObstacleBoard();
 		}
-	
+		
 }
-function getBoardUnit(row, col, className) {
+function initPlayerBoard(){
+	let board=document.createElement('table');
+	board.id="playerBoard";
+	console.log("getPlayerBoard");
+	initTable(board,9,'boardUnitPlayer');	
+	//initBoardEvents('#playerBoard');
+}
+function initObstacleBoard(){
+	let board=document.createElement('table');
+	board.id="obstacleBoard";
+	console.log("getObstacleBoard");
+	initTable(board,8,'boardUnitObstacle');
+	//initBoardEvents('#obstacleBoard');
+}
+
+function initTable(board,size,className){
+	for(let i=0;i<size;i++){
+		let boardRow = document.createElement('tr');
+		for(let j=0;j<size;j++){
+			let boardUnit = initBoardUnit(i,j,className);
+			boardRow.append(boardUnit);
+		}
+		board.append(boardRow);
+	}
+	document.body.append(board);
+}
+function initBoardUnit(row, col, className) {
 	let elem=document.createElement('td');
 	/*elem.id="bd"+i+j;*/
 	elem.setAttribute("data-row",row); //어차피문자열변환
@@ -41,4 +42,13 @@ function getBoardUnit(row, col, className) {
 	if(className == 'boardUnitPlayer'){ elem.innerHTML="("+row+", "+col+")"; }
 	elem.className=className;
 	return elem;
+}
+function initBoardEvents(id){
+	let tds = document.querySelectorAll('id td');
+		for(elem of tds){
+			elem.onclick = function(event){
+				let target = event.target;
+				console.log(target.getBoundingClientRect(), target.id,target.className);
+			}
+		}
 }
