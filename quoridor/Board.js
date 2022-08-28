a4fcc1e7529c5d20c1effc6204f708edcfe3e58a
박진우 <wlsdndml213@naver.com>
2022-08-28 13:18:49 +0900

    make 2 table

--- quoridor/Board.js
+++ quoridor/Board.js
@@ -3,21 +3,41 @@ class Board{
 
     }
     getBoardElement(){
-        let board=document.createElement('div');
-				board.id="board";
+        let board=document.createElement('table');
+				board.id="boardPlayer";
 				console.log("getBoarElement");
-				for(let i=0;i<8;i++){
+				for(let i=0;i<9;i++){
+					let boardRow = document.createElement('tr');
 					for(let j=0;j<9;j++){
-						let boardPlayerOn=document.createElement('div');
-						boardPlayerOn.className="bdPlayer";
-						boardPlayerOn.id="bd"+i+j;
-						boardPlayerOn.setAttribute("data-row",i); //어차피문자열변환
-						boardPlayerOn.setAttribute("data-col",j); //어차피문자열변환
+						let boardUnit=document.createElement('td');
+						boardUnit.id="bd"+i+j;
+						boardUnit.setAttribute("data-row",i); //어차피문자열변환
+						boardUnit.setAttribute("data-col",j); //어차피문자열변환
+						boardUnit.innerHTML="("+i+", "+j+")";
+						boardUnit.className="boardUnitPlayer";
+						boardRow.append(boardUnit);
 					}
+					board.append(boardRow);
 				}
 				document.body.append(board);
     }
-		test(){
-			console.log("TEST");
+		getCanvas(){
+			let board=document.createElement('table');
+				board.id="boardObstacle";
+				console.log("getInnerBoardElement");
+				for(let i=0;i<8;i++){
+					let boardRow = document.createElement('tr');
+					for(let j=0;j<8;j++){
+						let boardUnit=document.createElement('td');
+						boardUnit.id="bd"+i+j;
+						boardUnit.setAttribute("data-row",i); //어차피문자열변환
+						boardUnit.setAttribute("data-col",j); //어차피문자열변환
+						boardUnit.innerHTML="("+i+", "+j+")";
+						boardUnit.className="boardUnitObstacle";
+						boardRow.append(boardUnit);
+					}
+					board.append(boardRow);
+				}
+				document.body.append(board);
 		}
 }
